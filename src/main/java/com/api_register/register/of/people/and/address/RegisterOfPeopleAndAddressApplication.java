53a47b45b73978d9b7c9b64c1391f9931f81d947
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2025-02-18 10:40:17 -0300

    commit intial

---
+++ src/main/java/com/api_register/register/of/people/and/address/RegisterOfPeopleAndAddressApplication.java
@@ -0,0 +1,13 @@
+package com.api_register.register.of.people.and.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterOfPeopleAndAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterOfPeopleAndAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_register/register/of/people/and/address/RegisterOfPeopleAndAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_register.register.of.people.and.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterOfPeopleAndAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
