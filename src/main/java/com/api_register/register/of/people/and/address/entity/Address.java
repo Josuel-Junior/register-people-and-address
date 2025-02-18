b859e16e71fbd2a359b3e08149a87532aa20c56a
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2025-02-18 11:03:51 -0300

    created entity Address

---
+++ src/main/java/com/api_register/register/of/people/and/address/entity/Address.java
@@ -0,0 +1,65 @@
+package com.api_register.register.of.people.and.address.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_address")
+public class Address {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    private String city;
+    private String street;
+    private String zipCode;
+    private String number;
+
+    public Address() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
