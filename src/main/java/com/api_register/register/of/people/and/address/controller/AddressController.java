0d449a09055efde49340aafa3d18378060387196
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2025-02-24 18:02:56 -0300

    add swagger for API documentation

--- src/main/java/com/api_register/register/of/people/and/address/controller/AddressController.java
+++ src/main/java/com/api_register/register/of/people/and/address/controller/AddressController.java
@@ -3,6 +3,10 @@ package com.api_register.register.of.people.and.address.controller;
 import com.api_register.register.of.people.and.address.dto.AddressDto;
 import com.api_register.register.of.people.and.address.dto.PersonAddressDto;
 import com.api_register.register.of.people.and.address.service.AddressService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +21,23 @@ public class AddressController {
         this.addressService = addressService;
     }
 
+    @Operation(
+            summary = "Associa endereço à pessoa",
+            description = "Associa um endereço à pessoa identificada pelo ID. Se o endereço já existir, ele é vinculado; caso contrário, é criado e associado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Endereço associado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Pessoa não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     @PostMapping("{personId}")
-    public ResponseEntity<PersonAddressDto> save(@PathVariable("personId") String personId, @RequestBody AddressDto addressDto) {
+    public ResponseEntity<PersonAddressDto> save(
+            @Parameter(description = "ID da pessoa à qual o endereço será associado", required = true)
+            @PathVariable("personId") String personId,
+            @Parameter(description = "Dados do endereço a ser associado", required = true)
+            @RequestBody AddressDto addressDto) {
 
         var response = addressService.associateAddress(personId, addressDto);
-
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/api_register/register/of/people/and/address/controller/PersonController.java
+++ src/main/java/com/api_register/register/of/people/and/address/controller/PersonController.java
@@ -4,6 +4,10 @@ package com.api_register.register.of.people.and.address.controller;
 import com.api_register.register.of.people.and.address.dto.PersonDto;
 import com.api_register.register.of.people.and.address.entity.Person;
 import com.api_register.register.of.people.and.address.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,49 +27,67 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @Operation(summary = "Cria uma nova pessoa", description = "Cria uma nova pessoa a partir dos dados fornecidos no corpo da requisição.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pessoa criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    })
     @PostMapping
     public ResponseEntity<Person> save(@RequestBody PersonDto personDto) {
-
         var response = personService.createdPerson(personDto);
-
         return ResponseEntity.created(URI.create("./person/" + response.toString())).build();
-
     }
 
+    @Operation(summary = "Lista todas as pessoas", description = "Retorna a lista de todas as pessoas cadastradas.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de pessoas retornada com sucesso")
+    })
     @GetMapping
     public ResponseEntity<List<Person>> getAllPerons() {
-
         var responseList = personService.getAll();
-
         return ResponseEntity.ok(responseList);
     }
 
+    @Operation(summary = "Exclui uma pessoa", description = "Exclui a pessoa identificada pelo ID informado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Pessoa excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Pessoa não encontrada")
+    })
     @DeleteMapping("/{personId}")
-    public ResponseEntity<UUID> delete(@PathVariable("personId") String personId) {
-
+    public ResponseEntity<UUID> delete(
+            @Parameter(description = "ID da pessoa a ser excluída")
+            @PathVariable("personId") String personId) {
         personService.deleteById(personId);
-
         return ResponseEntity.noContent().build();
     }
 
-
+    @Operation(summary = "Busca uma pessoa por ID", description = "Retorna a pessoa correspondente ao ID informado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pessoa encontrada"),
+            @ApiResponse(responseCode = "404", description = "Pessoa não encontrada")
+    })
     @GetMapping("/{personId}")
-    public ResponseEntity<Person> getById(@PathVariable("personId") String personId) {
-
+    public ResponseEntity<Person> getById(
+            @Parameter(description = "ID da pessoa a ser buscada")
+            @PathVariable("personId") String personId) {
         var personOptional = personService.getById(personId);
-
         if (personOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-
         return ResponseEntity.ok(personOptional.get());
     }
 
+    @Operation(summary = "Atualiza os dados de uma pessoa", description = "Atualiza os dados da pessoa identificada pelo ID com as informações fornecidas.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pessoa atualizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Pessoa não encontrada")
+    })
     @PutMapping("/{personId}")
-    public ResponseEntity<Person> update(@PathVariable("personId") String personId, @RequestBody PersonDto personDto) {
-
+    public ResponseEntity<Person> update(
+            @Parameter(description = "ID da pessoa a ser atualizada")
+            @PathVariable("personId") String personId,
+            @RequestBody PersonDto personDto) {
         var responseUpdate = personService.updatePerson(personId, personDto);
-
         return ResponseEntity.ok(responseUpdate);
     }
 
